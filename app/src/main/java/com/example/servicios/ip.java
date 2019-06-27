4521765e52dc4334fb0ebdbd9c5a6c9082eb5f1e
ronalokd <ronalokd@gmail.com>
2019-06-26 21:07:54 -0400

    practica 5y6

---
+++ app/src/main/java/com/example/servicios/ip.java
@@ -0,0 +1,5 @@
+package com.example.servicios;
+
+public class ip {
+    public static final String localhost="http://192.168.43.220:8000/";
+}
---
+++ app/src/main/java/com/example/servicios/item.java
@@ -0,0 +1,12 @@
+package com.example.servicios;
+
+public class item {
+    private String url;
+    item(String u){
+        url=u;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
